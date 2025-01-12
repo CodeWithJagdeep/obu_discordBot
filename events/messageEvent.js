68e6df389cda26bbf0e32ca495466ea6e68adcfe
Jagdeep_singh <jagdeepsnh57@gmail.com>
2025-01-12 19:01:20 +0530

    Refactor message event handling and fix command parsing logic

---
+++ events/messageEvent.js
@@ -0,0 +1,34 @@
+const CommandsController = require("../Controllers/CommandsController");
+const { emotions } = require("../data/emotions");
+const { formatMessage } = require("../utils/dynamicMessage");
+
+const messageEvent = (client) => {
+  client.on("messageCreate", async (message) => {
+    if (message.author.bot) return; // Ignore bot messages
+    console.log(message);
+    const command = message.content.toLowerCase().trim();
+    if (command.startsWith("/")) {
+      let currentEvent = command.slice(1, message.content.length).trim();
+      if (currentEvent.toLowerCase() == "spam") {
+        this._handleSpamMessage();
+      }
+    }
+    if (message.content.toLowerCase().startsWith("obu")) {
+      let hasActionKey = emotions.filter(
+        (state) => message.content.toLowerCase().includes(state) // Return true if message contains the state
+      )[0];
+
+      if (hasActionKey) {
+        CommandsController.sendGif(message, hasActionKey);
+      } else if (message.content.toLowerCase().includes("commands")) {
+        CommandsController.obuCommands(message);
+      }
+    } else if (command.toLowerCase() == "spam") {
+      CommandsController._handleSpam(message);
+    } else {
+      await message.channel.send(formatMessage("Command not recognized."));
+    }
+  });
+};
+
+module.exports = messageEvent;
